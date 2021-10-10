b43805847965302324ea4182e8002a2bb584f3fc
evan <evan99mahony@gmail.com>
2021-10-10 04:59:03 +0100

    Messing with icons

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<title>Evan Mahony's Webpage</title>
 	<link rel="stylesheet" href="style.css">
 	<meta charset="utf-8">
 	<!-- Place this tag in your head or just before your close body tag. -->
 	<script async defer src="https://buttons.github.io/buttons.js"></script>
+	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -15,7 +15,8 @@
 	<a class="github-button" href="https://github.com/evanmahony"
 		data-color-scheme="no-preference: light; light: light; dark: dark;"
 		aria-label="Follow @evanmahony on GitHub">Follow @evanmahony</a>
-
+	<a href="google.com">
+		<i class="fab fa-linkedin"></a>
 	<p>The purpose of this website is to give a bit of information about myself and to publish articles about so of my
 		interests.</p>
 
