031c2007403e253ba2ed12577d29610c339bdadb
evan <evan99mahony@gmail.com>
2021-10-19 03:17:33 +0100

    Added in projects

---
+++ src/projects.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Projects</title>
+        <?php include 'head.html';?>
+	</head>
+
+	<body>
+		<?php include 'header.html';?>
+        <h2>Projects</h2>
+        <p>Projects I've undertaken are documented here.</p>
+        <ul>
+            <li>Optiver Volatility</li>
+            <li><a href=selfhostedwebsite.php>Self Hosted Website</a></li>
+            <li>Better Working World Data Challenge</li>
+            <li>Climathon 2021</li>
+            <li>Numerai</li>
+            <li>Terminal</li>
+		<?php include 'footer.html';?>
+	</body>
+</html>
